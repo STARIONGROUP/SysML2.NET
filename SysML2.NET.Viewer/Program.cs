c42142057aa65d99867ca066ce57fa5dd9628aaa
sam.gerene <s.gerene@rheagroup.com>
2022-08-02 14:04:34 +0200

    [Add] viewer project to solution

---
+++ SysML2.NET.Viewer/Program.cs
@@ -0,0 +1,79 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="RHEA System S.A.">
+//
+//   Copyright 2022 RHEA System S.A.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace SysML2.NET.Viewer
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    
+    using BlazorStrap;
+
+    using Microsoft.AspNetCore.Components.Web;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+    using Microsoft.Extensions.DependencyInjection;
+    
+    using Radzen;
+
+    using Serilog;
+    using Serilog.Events;
+
+    /// <summary>
+    /// The purpose of the <see cref="Program"/> class is to provide the
+    /// main entry point of te application
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main entry point of the application
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        /// <returns>
+        /// an awaitable <see cref="Task"/>
+        /// </returns>
+        public static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.BrowserConsole()
+                .CreateLogger();
+
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddLogging(loggingBuilder =>
+                loggingBuilder.AddSerilog(dispose: true));
+
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient());
+
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+            
+            builder.Services.AddBlazorStrap();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
