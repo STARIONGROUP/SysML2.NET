5702a4f79d9d662b03ea785a41014e0be51331b5
samatrhea <s.gerene@rheagroup.com>
2023-04-23 18:22:04 +0200

    [Fix] expected DictionaryReader and DictionaryWriter classes

--- SysML2.NET.CodeGenerator.Tests/Expected/AutoGenDictionaryReader/AnnotatingElementDictionaryReader.cs
+++ SysML2.NET.CodeGenerator.Tests/Expected/AutoGenDictionaryReader/AnnotatingElementDictionaryReader.cs
@@ -22,7 +22,7 @@
 // --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
 // ------------------------------------------------------------------------------------------------
 
-namespace SysML2.NET.Serializer.Dictionary
+namespace SysML2.NET.Serializer.Dictionary.Core.DTO
 {
     using System;
     using System.Collections.Generic;
@@ -282,7 +282,7 @@ namespace SysML2.NET.Serializer.Dictionary
             }
             var id = Guid.Parse(Convert.ToString(idObject));
 
-            var annotatingElementInstance = new Core.DTO.AnnotatingElement
+            var annotatingElementInstance = new SysML2.NET.Core.DTO.AnnotatingElement
             {
                 Id = id
             };
