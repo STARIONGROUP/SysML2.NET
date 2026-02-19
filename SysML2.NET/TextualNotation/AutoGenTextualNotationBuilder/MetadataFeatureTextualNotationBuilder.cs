6351c9aeef6a80e9b953c50ae09c4b794d8a93dd
atheate <a.theate@stariongroup.eu>
2026-02-19 15:36:57 +0100

    [WIP] First draft of Code-Gen

---
+++ SysML2.NET/TextualNotation/AutoGenTextualNotationBuilder/MetadataFeatureTextualNotationBuilder.cs
@@ -0,0 +1,56 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="MetadataFeatureTextualNotationBuilder.cs" company="Starion Group S.A.">
+//
+//   Copyright 2022-2026 Starion Group S.A.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
+
+namespace SysML2.NET.TextualNotation
+{
+    using SysML2.NET.Core.POCO.Root.Elements;
+
+    /// <summary>
+    /// The <see cref="MetadataFeatureTextualNotationBuilder" /> provides Textual Notation Builder for the <see cref="SysML2.NET.Core.POCO.Kernel.Metadata.MetadataFeature" /> element
+    /// </summary>
+    public class MetadataFeatureTextualNotationBuilder : TextualNotationBuilder<SysML2.NET.Core.POCO.Kernel.Metadata.MetadataFeature>
+    {
+        /// <summary>
+        /// Initializes a new instance of a <see cref="NamespaceTextualNotationBuilder"/>
+        /// </summary>
+        /// <param name="facade">The <see cref="ITextualNotationBuilderFacade"/> used to query textual notation of referenced <see cref="IElement"/></param>
+        public MetadataFeatureTextualNotationBuilder(ITextualNotationBuilderFacade facade) : base(facade)
+        {
+        }
+
+        /// <summary>
+        /// Builds the Textual Notation string for the provided <see cref="SysML2.NET.Core.POCO.Kernel.Metadata.MetadataFeature"/>
+        /// </summary>
+        /// <param name="poco">The <see cref="SysML2.NET.Core.POCO.Kernel.Metadata.MetadataFeature"/> from which the textual notation should be build</param>
+        /// <returns>The built textual notation string</returns>
+        public override string BuildTextualNotation(SysML2.NET.Core.POCO.Kernel.Metadata.MetadataFeature poco)
+        {
+            return string.Empty;
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
---
+++ SysML2.NET/TextualNotation/TextualNotationBuilder.cs
@@ -0,0 +1,52 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="TextualNotationBuilder.cs" company="Starion Group S.A.">
+// 
+//   Copyright 2022-2026 Starion Group S.A.
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+namespace SysML2.NET.TextualNotation
+{
+    using SysML2.NET.Core.POCO.Root.Elements;
+
+    /// <summary>
+    /// Base class that provides Textual Notation string builder for a specific <typeparamref name="TElement"/>
+    /// </summary>
+    /// <typeparam name="TElement">Any <see cref="TElement"/> concrete class</typeparam>
+    public abstract class TextualNotationBuilder<TElement> where TElement : class, IElement
+    {
+        /// <summary>
+        /// Gets the <see cref="ITextualNotationBuilderFacade"/> used to query textual notation of referenced <see cref="IElement"/>
+        /// </summary>
+        protected readonly ITextualNotationBuilderFacade Facade;
+
+        /// <summary>
+        /// Initializes a new instance of a <see cref="TextualNotationBuilder{TElement}"/>
+        /// </summary>
+        /// <param name="facade">The <see cref="ITextualNotationBuilderFacade"/> used to query textual notation of referenced <see cref="IElement"/></param>
+        protected TextualNotationBuilder(ITextualNotationBuilderFacade facade)
+        {
+            this.Facade = facade;
+        }
+
+        /// <summary>
+        /// Builds the Textual Notation string for the provided <typeparamref name="TElement" />
+        /// </summary>
+        /// <param name="poco">The <typeparamref name="TElement" /> from which the textual notation should be build</param>
+        /// <returns>The built textual notation string</returns>
+        public abstract string BuildTextualNotation(TElement poco);
+    }
+}
