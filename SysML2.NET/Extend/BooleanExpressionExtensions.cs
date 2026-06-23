a42ee93010a6ec13d872e05046bc278e03bc0649
TanaseMariusatstarion <tm.manafu@stariongroup.eu>
2026-06-23 17:28:02 +0200

    102 feature implement booleanexpressionextensions (#309)
    * BooleanExpressionExtension.cs defined
    
    * BooleanExpressionExtension.cs test defined
    
    * BooleanExpressionExtension.cs correction for multiple types within booleanExpressionSubject

--- SysML2.NET/Extend/BooleanExpressionExtensions.cs
+++ SysML2.NET/Extend/BooleanExpressionExtensions.cs
@@ -1,40 +1,32 @@
 ﻿// -------------------------------------------------------------------------------------------------
 // <copyright file="BooleanExpressionExtensions.cs" company="Starion Group S.A.">
-//
-//    Copyright (C) 2022-2026 Starion Group S.A.
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
+// 
+//   Copyright (C) 2022-2026 Starion Group S.A.
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
 //        http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-//
+// 
 // </copyright>
 // ------------------------------------------------------------------------------------------------
 
 namespace SysML2.NET.Core.POCO.Kernel.Functions
 {
     using System;
-    using System.Collections.Generic;
-
-    using SysML2.NET.Core.Core.Types;
-    using SysML2.NET.Core.Root.Namespaces;
-    using SysML2.NET.Core.POCO.Core.Features;
-    using SysML2.NET.Core.POCO.Core.Types;
-    using SysML2.NET.Core.POCO.Kernel.Behaviors;
-    using SysML2.NET.Core.POCO.Root.Annotations;
-    using SysML2.NET.Core.POCO.Root.Elements;
-    using SysML2.NET.Core.POCO.Root.Namespaces;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
 
     /// <summary>
-    /// The <see cref="BooleanExpressionExtensions"/> class provides extensions methods for
-    /// the <see cref="IBooleanExpression"/> interface
+    /// The <see cref="BooleanExpressionExtensions" /> class provides extensions methods for
+    /// the <see cref="IBooleanExpression" /> interface
     /// </summary>
     internal static class BooleanExpressionExtensions
     {
@@ -42,16 +34,17 @@ namespace SysML2.NET.Core.POCO.Kernel.Functions
         /// Computes the derived property.
         /// </summary>
         /// <param name="booleanExpressionSubject">
-        /// The subject <see cref="IBooleanExpression"/>
+        /// The subject <see cref="IBooleanExpression" />
         /// </param>
         /// <returns>
         /// the computed result
         /// </returns>
-        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+        [ExcludeFromCodeCoverage]
         internal static IPredicate ComputePredicate(this IBooleanExpression booleanExpressionSubject)
         {
-            throw new NotSupportedException("Create a GitHub issue when this method is required");
+            return booleanExpressionSubject == null
+                ? throw new ArgumentNullException(nameof(booleanExpressionSubject))
+                : booleanExpressionSubject.type.OfType<IPredicate>().FirstOrDefault();
         }
-
     }
 }
