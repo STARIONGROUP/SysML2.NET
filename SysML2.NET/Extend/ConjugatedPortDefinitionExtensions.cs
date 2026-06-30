603c173c85888d04032837b5b583c955cddffc26
TanaseMariusatstarion <tm.manafu@stariongroup.eu>
2026-06-30 23:03:20 +0200

    implemented ConjugatedPortDefinitionExtensions and corresponding tests

--- SysML2.NET/Extend/ConjugatedPortDefinitionExtensions.cs
+++ SysML2.NET/Extend/ConjugatedPortDefinitionExtensions.cs
@@ -1,63 +1,34 @@
 ﻿// -------------------------------------------------------------------------------------------------
 // <copyright file="ConjugatedPortDefinitionExtensions.cs" company="Starion Group S.A.">
-//
-//    Copyright (C) 2022-2026 Starion Group S.A.
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
+// 
+//   Copyright (C) 2022-2026 Starion Group S.A.
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
 //        http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-//
+// 
 // </copyright>
 // ------------------------------------------------------------------------------------------------
 
 namespace SysML2.NET.Core.POCO.Systems.Ports
 {
     using System;
-    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
     using SysML2.NET.Exceptions;
     using SysML2.NET.Extensions;
 
-    using SysML2.NET.Core.Core.Types;
-    using SysML2.NET.Core.Root.Namespaces;
-    using SysML2.NET.Core.POCO.Core.Classifiers;
-    using SysML2.NET.Core.POCO.Core.Features;
-    using SysML2.NET.Core.POCO.Core.Types;
-    using SysML2.NET.Core.POCO.Root.Annotations;
-    using SysML2.NET.Core.POCO.Root.Elements;
-    using SysML2.NET.Core.POCO.Root.Namespaces;
-    using SysML2.NET.Core.POCO.Systems.Actions;
-    using SysML2.NET.Core.POCO.Systems.Allocations;
-    using SysML2.NET.Core.POCO.Systems.AnalysisCases;
-    using SysML2.NET.Core.POCO.Systems.Attributes;
-    using SysML2.NET.Core.POCO.Systems.Calculations;
-    using SysML2.NET.Core.POCO.Systems.Cases;
-    using SysML2.NET.Core.POCO.Systems.Connections;
-    using SysML2.NET.Core.POCO.Systems.Constraints;
-    using SysML2.NET.Core.POCO.Systems.DefinitionAndUsage;
-    using SysML2.NET.Core.POCO.Systems.Enumerations;
-    using SysML2.NET.Core.POCO.Systems.Flows;
-    using SysML2.NET.Core.POCO.Systems.Interfaces;
-    using SysML2.NET.Core.POCO.Systems.Items;
-    using SysML2.NET.Core.POCO.Systems.Metadata;
-    using SysML2.NET.Core.POCO.Systems.Occurrences;
-    using SysML2.NET.Core.POCO.Systems.Parts;
-    using SysML2.NET.Core.POCO.Systems.Requirements;
-    using SysML2.NET.Core.POCO.Systems.States;
-    using SysML2.NET.Core.POCO.Systems.UseCases;
-    using SysML2.NET.Core.POCO.Systems.VerificationCases;
-    using SysML2.NET.Core.POCO.Systems.Views;
-
     /// <summary>
-    /// The <see cref="ConjugatedPortDefinitionExtensions"/> class provides extensions methods for
-    /// the <see cref="IConjugatedPortDefinition"/> interface
+    /// The <see cref="ConjugatedPortDefinitionExtensions" /> class provides extensions methods for
+    /// the <see cref="IConjugatedPortDefinition" /> interface
     /// </summary>
     internal static class ConjugatedPortDefinitionExtensions
     {
@@ -65,7 +36,7 @@ namespace SysML2.NET.Core.POCO.Systems.Ports
         /// Computes the derived property.
         /// </summary>
         /// <param name="conjugatedPortDefinitionSubject">
-        /// The subject <see cref="IConjugatedPortDefinition"/>
+        /// The subject <see cref="IConjugatedPortDefinition" />
         /// </param>
         /// <returns>
         /// the computed result
@@ -86,7 +57,7 @@ namespace SysML2.NET.Core.POCO.Systems.Ports
         /// Computes the derived property.
         /// </summary>
         /// <param name="conjugatedPortDefinitionSubject">
-        /// The subject <see cref="IConjugatedPortDefinition"/>
+        /// The subject <see cref="IConjugatedPortDefinition" />
         /// </param>
         /// <returns>
         /// the computed result
@@ -99,7 +70,7 @@ namespace SysML2.NET.Core.POCO.Systems.Ports
             }
 
             return conjugatedPortDefinitionSubject.OwnedRelationship
-                .RequireSingleOfType<IPortConjugation>(nameof(conjugatedPortDefinitionSubject));
+                .SingleStrict<IPortConjugation>(nameof(conjugatedPortDefinitionSubject));
         }
 
         /// <summary>
@@ -116,12 +87,12 @@ namespace SysML2.NET.Core.POCO.Systems.Ports
         /// </code>
         /// </remarks>
         /// <param name="conjugatedPortDefinitionSubject">
-        /// The subject <see cref="IConjugatedPortDefinition"/>
+        /// The subject <see cref="IConjugatedPortDefinition" />
         /// </param>
         /// <returns>
         /// The expected <see cref="string" />
         /// </returns>
-        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+        [ExcludeFromCodeCoverage]
         internal static string ComputeRedefinedEffectiveNameOperation(this IConjugatedPortDefinition conjugatedPortDefinitionSubject)
         {
             throw new NotSupportedException("Create a GitHub issue when this method is required");
