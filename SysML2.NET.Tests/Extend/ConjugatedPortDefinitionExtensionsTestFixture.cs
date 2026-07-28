c6e02d8b99039d96b9c4020a098572a31d216e8c
TanaseMariusatstarion <tm.manafu@stariongroup.eu>
2026-07-28 11:04:15 +0200

    109 implement conjugated portdefinitionextensions (#320)
    * implemented computeownedportconjugated in conjugatedportdefinitionextensions
    
    * implemented ConjugatedPortDefinitionExtensions and corresponding tests
    
    * implemented third method of ConjugatedPortDefinitionExtensions with corresponding test

--- SysML2.NET.Tests/Extend/ConjugatedPortDefinitionExtensionsTestFixture.cs
+++ SysML2.NET.Tests/Extend/ConjugatedPortDefinitionExtensionsTestFixture.cs
@@ -1,7 +1,7 @@
 ﻿// -------------------------------------------------------------------------------------------------
 // <copyright file="ConjugatedPortDefinitionExtensionsTestFixture.cs" company="Starion Group S.A.">
 // 
-//   Copyright 2022-2026 Starion Group S.A.
+//   Copyright (C) 2022-2026 Starion Group S.A.
 // 
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -21,24 +21,86 @@
 namespace SysML2.NET.Tests.Extend
 {
     using System;
-    
+
     using NUnit.Framework;
-    
+
+    using SysML2.NET.Core.POCO.Root.Elements;
+    using SysML2.NET.Core.POCO.Root.Namespaces;
     using SysML2.NET.Core.POCO.Systems.Ports;
+    using SysML2.NET.Exceptions;
+    using SysML2.NET.Extensions;
 
     [TestFixture]
     public class ConjugatedPortDefinitionExtensionsTestFixture
     {
         [Test]
-        public void ComputeOriginalPortDefinition_ThrowsNotSupportedException()
+        public void VerifyComputeOriginalPortDefinition()
         {
-            Assert.That(() => ((IConjugatedPortDefinition)null).ComputeOriginalPortDefinition(), Throws.TypeOf<NotSupportedException>());
+            // Null Subject:
+            Assert.That(() => ((IConjugatedPortDefinition)null).ComputeOriginalPortDefinition(), Throws.TypeOf<ArgumentNullException>());
+
+            // Missing Owning Membership:
+            var emptyConjugatedPortDefinition = new ConjugatedPortDefinition();
+            Assert.That(() => emptyConjugatedPortDefinition.ComputeOriginalPortDefinition(), Throws.TypeOf<IncompleteModelException>());
+
+            // With Original Port Definition:
+            var originalPortDefinition = new PortDefinition();
+            var conjugatedPortDefinition = new ConjugatedPortDefinition();
+            originalPortDefinition.AssignOwnership(new OwningMembership(), conjugatedPortDefinition);
+            Assert.That(conjugatedPortDefinition.ComputeOriginalPortDefinition(), Is.SameAs(originalPortDefinition));
+
+            // Wrong Owner Type:
+            var nonPortDefinitionOwner = new Namespace();
+            var conjugatedWithNonPortDefinitionOwner = new ConjugatedPortDefinition();
+            nonPortDefinitionOwner.AssignOwnership(new OwningMembership(), conjugatedWithNonPortDefinitionOwner);
+            Assert.That(() => conjugatedWithNonPortDefinitionOwner.ComputeOriginalPortDefinition(), Throws.TypeOf<IncompleteModelException>());
+        }
+
+        [Test]
+        public void VerifyComputeOwnedPortConjugator()
+        {
+            // Null Subject:
+            Assert.That(() => ((IConjugatedPortDefinition)null).ComputeOwnedPortConjugator(), Throws.TypeOf<ArgumentNullException>());
+
+            // Missing Port Conjugation:
+            var emptyConjugatedPortDefinition = new ConjugatedPortDefinition();
+            Assert.That(() => emptyConjugatedPortDefinition.ComputeOwnedPortConjugator(), Throws.TypeOf<IncompleteModelException>());
+
+            // With Owned Port Conjugator:
+            var conjugatedPortDefinition = new ConjugatedPortDefinition();
+            var portConjugation = new PortConjugation();
+            ((IContainedElement)conjugatedPortDefinition).OwnedRelationship.Add(portConjugation);
+            Assert.That(conjugatedPortDefinition.ComputeOwnedPortConjugator(), Is.SameAs(portConjugation));
+
+            // Multiple Port Conjugations:
+            var conjugatedWithMultiplePortConjugations = new ConjugatedPortDefinition();
+            ((IContainedElement)conjugatedWithMultiplePortConjugations).OwnedRelationship.Add(new PortConjugation());
+            ((IContainedElement)conjugatedWithMultiplePortConjugations).OwnedRelationship.Add(new PortConjugation());
+            Assert.That(() => conjugatedWithMultiplePortConjugations.ComputeOwnedPortConjugator(), Throws.TypeOf<MultiplicityViolationException>());
         }
         
         [Test]
-        public void ComputeOwnedPortConjugator_ThrowsNotSupportedException()
+        public void VerifyComputeRedefinedEffectiveNameOperation()
         {
-            Assert.That(() => ((IConjugatedPortDefinition)null).ComputeOwnedPortConjugator(), Throws.TypeOf<NotSupportedException>());
+            // Null subject:
+            Assert.That(() => ((IConjugatedPortDefinition)null).ComputeRedefinedEffectiveNameOperation(), Throws.TypeOf<ArgumentNullException>());
+
+            // Missing original port definition:
+            var emptyConjugatedPortDefinition = new ConjugatedPortDefinition();
+            Assert.That(() => emptyConjugatedPortDefinition.ComputeRedefinedEffectiveNameOperation(), Throws.TypeOf<IncompleteModelException>());
+
+            // Original name is null:
+            var originalPortDefinitionWithoutName = new PortDefinition();
+            var conjugatedPortDefinitionWithoutOriginalName = new ConjugatedPortDefinition();
+            originalPortDefinitionWithoutName.AssignOwnership(new OwningMembership(), conjugatedPortDefinitionWithoutOriginalName);
+            Assert.That(conjugatedPortDefinitionWithoutOriginalName.ComputeRedefinedEffectiveNameOperation(), Is.Null);
+
+            // Original name exists:
+            var originalPortDefinition = new PortDefinition { DeclaredName = "port" };
+            var conjugatedPortDefinition = new ConjugatedPortDefinition();
+            originalPortDefinition.AssignOwnership(new OwningMembership(), conjugatedPortDefinition);
+            Assert.That(conjugatedPortDefinition.ComputeRedefinedEffectiveNameOperation(), Is.EqualTo("~port"));
         }
+        
     }
 }
