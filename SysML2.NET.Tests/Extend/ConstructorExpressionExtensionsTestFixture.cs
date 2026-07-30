905ac9a6ad86b9143fd56dc18f2e14e7901a177c
Antoine Théate <97221392+antoineatstariongroup@users.noreply.github.com>
2026-07-30 14:33:39 +0200

    Batch impl extensions 146 (#325)
    * Fix #146 and OperatorExpressionExtensions
    
    * removes todo
    
    * code coverage

--- SysML2.NET.Tests/Extend/ConstructorExpressionExtensionsTestFixture.cs
+++ SysML2.NET.Tests/Extend/ConstructorExpressionExtensionsTestFixture.cs
@@ -1,20 +1,20 @@
 // -------------------------------------------------------------------------------------------------
 // <copyright file="ConstructorExpressionExtensionsTestFixture.cs" company="Starion Group S.A.">
-//
-//   Copyright 2022-2026 Starion Group S.A.
-//
+// 
+//   Copyright (C) 2022-2026 Starion Group S.A.
+// 
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
-//
+// 
 //        http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-//
+// 
 // </copyright>
 // ------------------------------------------------------------------------------------------------
 
@@ -24,7 +24,14 @@ namespace SysML2.NET.Tests.Extend
 
     using NUnit.Framework;
 
+    using SysML2.NET.Core.POCO.Core.Features;
+    using SysML2.NET.Core.POCO.Core.Types;
+    using SysML2.NET.Core.POCO.Kernel.Behaviors;
     using SysML2.NET.Core.POCO.Kernel.Expressions;
+    using SysML2.NET.Core.POCO.Kernel.FeatureValues;
+    using SysML2.NET.Core.POCO.Kernel.Functions;
+    using SysML2.NET.Core.POCO.Root.Namespaces;
+    using SysML2.NET.Extensions;
 
     [TestFixture]
     public class ConstructorExpressionExtensionsTestFixture
@@ -32,16 +39,57 @@ namespace SysML2.NET.Tests.Extend
         [Test]
         public void VerifyComputeRedefinedModelLevelEvaluableOperation()
         {
+            // Null guard.
             Assert.That(
                 () => ((IConstructorExpression)null).ComputeRedefinedModelLevelEvaluableOperation([]),
                 Throws.TypeOf<ArgumentNullException>());
 
-            var subject = new ConstructorExpression();
+            // Empty arguments: instantiatedType is unresolved -> ComputeArgument returns [] ->
+            // All(...) over an empty source -> true.
+            var emptySubject = new ConstructorExpression();
 
-            // For later: populated case depends on ComputeArgument, still a stub (out of batch).
-            Assert.That(
-                () => subject.ComputeRedefinedModelLevelEvaluableOperation([]),
-                Throws.TypeOf<NotSupportedException>());
+            // False branch: a single argument whose ModelLevelEvaluable is deterministically false
+            // (a base Expression carrying a non-implied Specialization) -> All(...) -> false.
+            var falseArgument = new Expression();
+            falseArgument.AssignOwnership(new Specialization { IsImplied = false, Specific = falseArgument });
+            var falseSubject = BuildConstructor(falseArgument);
+
+            // True branch: a single LiteralInteger argument (a LiteralExpression is always
+            // model-level evaluable) -> All(...) -> true.
+            var trueArgument = new LiteralInteger();
+            var trueSubject = BuildConstructor(trueArgument);
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(emptySubject.ComputeRedefinedModelLevelEvaluableOperation([]), Is.True);
+                Assert.That(falseSubject.ComputeRedefinedModelLevelEvaluableOperation([]), Is.False);
+                Assert.That(trueSubject.ComputeRedefinedModelLevelEvaluableOperation([]), Is.True);
+            }
+
+            // Builds a ConstructorExpression whose single ComputeArgument-derived argument is the supplied
+            // expression: a constructed Behavior owns a feature, redefined by a result-owned feature whose
+            // FeatureValue holds the argument (mirrors the constructor-family scaffolding in
+            // InstantiationExpressionExtensionsTestFixture.VerifyComputeArgument).
+            static ConstructorExpression BuildConstructor(IExpression argument)
+            {
+                var constructor = new ConstructorExpression();
+
+                var constructedBehavior = new Behavior();
+                constructor.AssignOwnership(new OwningMembership(), constructedBehavior);
+
+                var constructedFeature = new Feature();
+                constructedBehavior.AssignOwnership(new FeatureMembership(), constructedFeature);
+
+                var resultParameter = new Feature();
+                constructor.AssignOwnership(new ReturnParameterMembership(), resultParameter);
+
+                var resultRedefiningFeature = new Feature();
+                resultRedefiningFeature.AssignOwnership(new Redefinition { RedefinedFeature = constructedFeature });
+                resultRedefiningFeature.AssignOwnership(new FeatureValue(), argument);
+                resultParameter.AssignOwnership(new FeatureMembership(), resultRedefiningFeature);
+
+                return constructor;
+            }
         }
     }
 }
--- SysML2.NET/Extend/InstantiationExpressionExtensions.cs
+++ SysML2.NET/Extend/InstantiationExpressionExtensions.cs
@@ -1,20 +1,20 @@
 ﻿// -------------------------------------------------------------------------------------------------
 // <copyright file="InstantiationExpressionExtensions.cs" company="Starion Group S.A.">
-//
-//    Copyright (C) 2022-2026 Starion Group S.A.
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
+// 
+//   Copyright (C) 2022-2026 Starion Group S.A.
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
 //        http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-//
+// 
 // </copyright>
 // ------------------------------------------------------------------------------------------------
 
@@ -22,20 +22,16 @@ namespace SysML2.NET.Core.POCO.Kernel.Expressions
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
-    using SysML2.NET.Core.Core.Types;
-    using SysML2.NET.Core.Root.Namespaces;
     using SysML2.NET.Core.POCO.Core.Features;
     using SysML2.NET.Core.POCO.Core.Types;
-    using SysML2.NET.Core.POCO.Kernel.Behaviors;
+    using SysML2.NET.Core.POCO.Kernel.FeatureValues;
     using SysML2.NET.Core.POCO.Kernel.Functions;
-    using SysML2.NET.Core.POCO.Root.Annotations;
-    using SysML2.NET.Core.POCO.Root.Elements;
-    using SysML2.NET.Core.POCO.Root.Namespaces;
 
     /// <summary>
-    /// The <see cref="InstantiationExpressionExtensions"/> class provides extensions methods for
-    /// the <see cref="IInstantiationExpression"/> interface
+    /// The <see cref="InstantiationExpressionExtensions" /> class provides extensions methods for
+    /// the <see cref="IInstantiationExpression" /> interface
     /// </summary>
     internal static class InstantiationExpressionExtensions
     {
@@ -43,15 +39,39 @@ namespace SysML2.NET.Core.POCO.Kernel.Expressions
         /// Computes the derived property.
         /// </summary>
         /// <param name="instantiationExpressionSubject">
-        /// The subject <see cref="IInstantiationExpression"/>
+        /// The subject <see cref="IInstantiationExpression" />
         /// </param>
         /// <returns>
         /// the computed result
         /// </returns>
-        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
         internal static List<IExpression> ComputeArgument(this IInstantiationExpression instantiationExpressionSubject)
         {
-            throw new NotSupportedException("Create a GitHub issue when this method is required");
+            if (instantiationExpressionSubject == null)
+            {
+                throw new ArgumentNullException(nameof(instantiationExpressionSubject));
+            }
+
+            var instantiatedType = instantiationExpressionSubject.instantiatedType;
+
+            if (instantiatedType == null)
+            {
+                return [];
+            }
+
+            return instantiationExpressionSubject is IConstructorExpression constructor
+                ? [
+                    .. instantiatedType.feature.SelectMany(f =>
+                        constructor.result.ownedFeature.Where(of => of.Redefines(f)).Select(ValueOf).Where(value => value != null))
+                ]
+                : [
+                    .. instantiatedType.input.SelectMany(inp =>
+                        instantiationExpressionSubject.ownedFeature.Where(of => of.Redefines(inp)).Select(ValueOf).Where(value => value != null))
+                ];
+
+            static IExpression ValueOf(IFeature feature)
+            {
+                return feature.ownedMembership.OfType<IFeatureValue>().FirstOrDefault()?.value;
+            }
         }
 
         /// <summary>
@@ -64,15 +84,19 @@ namespace SysML2.NET.Core.POCO.Kernel.Expressions
         /// </code>
         /// </remarks>
         /// <param name="instantiationExpressionSubject">
-        /// The subject <see cref="IInstantiationExpression"/>
+        /// The subject <see cref="IInstantiationExpression" />
         /// </param>
         /// <returns>
         /// the computed result
         /// </returns>
-        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
         internal static IType ComputeInstantiatedType(this IInstantiationExpression instantiationExpressionSubject)
         {
-            throw new NotSupportedException("Create a GitHub issue when this method is required");
+            if (instantiationExpressionSubject == null)
+            {
+                throw new ArgumentNullException(nameof(instantiationExpressionSubject));
+            }
+
+            return instantiationExpressionSubject.InstantiatedType();
         }
 
         /// <summary>
@@ -92,15 +116,22 @@ namespace SysML2.NET.Core.POCO.Kernel.Expressions
         /// </code>
         /// </remarks>
         /// <param name="instantiationExpressionSubject">
-        /// The subject <see cref="IInstantiationExpression"/>
+        /// The subject <see cref="IInstantiationExpression" />
         /// </param>
         /// <returns>
         /// The expected <see cref="IType" />
         /// </returns>
-        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
         internal static IType ComputeInstantiatedTypeOperation(this IInstantiationExpression instantiationExpressionSubject)
         {
-            throw new NotSupportedException("Create a GitHub issue when this method is required");
+            if (instantiationExpressionSubject == null)
+            {
+                throw new ArgumentNullException(nameof(instantiationExpressionSubject));
+            }
+
+            return instantiationExpressionSubject.ownedMembership
+                .Where(membership => membership is not IFeatureMembership)
+                .Select(membership => membership.MemberElement)
+                .FirstOrDefault() as IType;
         }
     }
 }
