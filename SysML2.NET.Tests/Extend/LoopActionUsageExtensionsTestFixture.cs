fbaaa7d29465097530b7b80b0e0d0c1730b20ee3
atheate <a.theate@stariongroup.eu>
2026-07-31 14:54:45 +0200

    Fix #140 #151 #163 #206

--- SysML2.NET.Tests/Extend/LoopActionUsageExtensionsTestFixture.cs
+++ SysML2.NET.Tests/Extend/LoopActionUsageExtensionsTestFixture.cs
@@ -1,38 +1,64 @@
-﻿// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 // <copyright file="LoopActionUsageExtensionsTestFixture.cs" company="Starion Group S.A.">
-// 
+//
 //   Copyright 2022-2026 Starion Group S.A.
-// 
+//
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-// 
+//
 // </copyright>
 // ------------------------------------------------------------------------------------------------
 
 namespace SysML2.NET.Tests.Extend
 {
     using System;
-    
+
     using NUnit.Framework;
-    
+
+    using SysML2.NET.Core.Core.Types;
+    using SysML2.NET.Core.POCO.Core.Types;
     using SysML2.NET.Core.POCO.Systems.Actions;
+    using SysML2.NET.Core.POCO.Systems.DefinitionAndUsage;
+    using SysML2.NET.Extensions;
 
     [TestFixture]
     public class LoopActionUsageExtensionsTestFixture
     {
         [Test]
-        public void ComputeBodyAction_ThrowsNotSupportedException()
+        public void VerifyComputeBodyAction()
         {
-            Assert.That(() => ((ILoopActionUsage)null).ComputeBodyAction(), Throws.TypeOf<NotSupportedException>());
+            // LoopActionUsage is abstract, so the concrete ILoopActionUsage subtype WhileLoopActionUsage is used as subject.
+            Assert.That(() => ((ILoopActionUsage)null).ComputeBodyAction(), Throws.TypeOf<ArgumentNullException>());
+
+            // No input parameters -> inputParameter(2) is out of range -> null.
+            Assert.That(new WhileLoopActionUsage().ComputeBodyAction(), Is.Null);
+
+            // Position-2 input parameter that IS an ActionUsage -> returned via the as-cast.
+            var subjectWithBodyAction = new WhileLoopActionUsage();
+            var bodyActionParameter = new ActionUsage { Direction = FeatureDirectionKind.In };
+            subjectWithBodyAction.AssignOwnership(new FeatureMembership(), new ReferenceUsage { Direction = FeatureDirectionKind.In });
+            subjectWithBodyAction.AssignOwnership(new FeatureMembership(), bodyActionParameter);
+
+            // Position-2 input parameter that is NOT an ActionUsage -> the as-cast yields null.
+            var subjectWithNonActionSecondParameter = new WhileLoopActionUsage();
+            subjectWithNonActionSecondParameter.AssignOwnership(new FeatureMembership(), new ReferenceUsage { Direction = FeatureDirectionKind.In });
+            subjectWithNonActionSecondParameter.AssignOwnership(new FeatureMembership(), new ReferenceUsage { Direction = FeatureDirectionKind.In });
+
+            using (Assert.EnterMultipleScope())
+            {
+                // bodyAction = inputParameter(2) as ActionUsage
+                Assert.That(subjectWithBodyAction.ComputeBodyAction(), Is.SameAs(bodyActionParameter));
+                Assert.That(subjectWithNonActionSecondParameter.ComputeBodyAction(), Is.Null);
+            }
         }
     }
 }
--- SysML2.NET.Tests/Extend/PerformActionUsageExtensionsTestFixture.cs
+++ SysML2.NET.Tests/Extend/PerformActionUsageExtensionsTestFixture.cs
@@ -1,38 +1,82 @@
-﻿// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 // <copyright file="PerformActionUsageExtensionsTestFixture.cs" company="Starion Group S.A.">
-// 
+//
 //   Copyright 2022-2026 Starion Group S.A.
-// 
+//
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
 //   You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-// 
+//
 // </copyright>
 // ------------------------------------------------------------------------------------------------
 
 namespace SysML2.NET.Tests.Extend
 {
     using System;
-    
+
     using NUnit.Framework;
-    
+
+    using SysML2.NET.Core.POCO.Core.Features;
     using SysML2.NET.Core.POCO.Systems.Actions;
+    using SysML2.NET.Core.POCO.Systems.Attributes;
+    using SysML2.NET.Core.POCO.Systems.DefinitionAndUsage;
+    using SysML2.NET.Extensions;
 
     [TestFixture]
     public class PerformActionUsageExtensionsTestFixture
     {
         [Test]
-        public void ComputePerformedAction_ThrowsNotSupportedException()
+        public void VerifyComputePerformedAction()
         {
-            Assert.That(() => ((IPerformActionUsage)null).ComputePerformedAction(), Throws.TypeOf<NotSupportedException>());
+            Assert.That(() => ((IPerformActionUsage)null).ComputePerformedAction(), Throws.TypeOf<ArgumentNullException>());
+
+            // No ownedReferenceSubsetting -> referencedFeatureTarget() is null -> returns self.
+            var subjectNoReferent = new PerformActionUsage();
+
+            Assert.That(subjectNoReferent.ComputePerformedAction(), Is.SameAs(subjectNoReferent));
+
+            // ReferenceSubsetting whose referent IS an ActionUsage -> returns that ActionUsage.
+            var subjectWithActionReferent = new PerformActionUsage();
+            var actionReferent = new ActionUsage();
+            subjectWithActionReferent.AssignOwnership(new ReferenceSubsetting { ReferencedFeature = actionReferent });
+
+            // ReferenceSubsetting whose referent is NOT an ActionUsage -> the as-cast yields null.
+            var subjectWithNonActionReferent = new PerformActionUsage();
+            subjectWithNonActionReferent.AssignOwnership(new ReferenceSubsetting { ReferencedFeature = new AttributeUsage() });
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(subjectWithActionReferent.ComputePerformedAction(), Is.SameAs(actionReferent));
+                Assert.That(subjectWithNonActionReferent.ComputePerformedAction(), Is.Null);
+            }
+        }
+
+        [Test]
+        public void VerifyComputeRedefinedNamingFeatureOperation()
+        {
+            Assert.That(() => ((IPerformActionUsage)null).ComputeRedefinedNamingFeatureOperation(), Throws.TypeOf<ArgumentNullException>());
+
+            // performedAction <> self (an ActionUsage referent) -> returns that performedAction.
+            var subjectWithActionReferent = new PerformActionUsage();
+            var actionReferent = new ActionUsage();
+            subjectWithActionReferent.AssignOwnership(new ReferenceSubsetting { ReferencedFeature = actionReferent });
+
+            // performedAction == self (no referent) -> falls through to the Usage-level namingFeature.
+            var subjectNoReferent = new PerformActionUsage();
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(subjectWithActionReferent.ComputeRedefinedNamingFeatureOperation(), Is.SameAs(actionReferent));
+                Assert.That(subjectNoReferent.ComputeRedefinedNamingFeatureOperation(), Is.EqualTo(UsageExtensions.ComputeRedefinedNamingFeatureOperation(subjectNoReferent)));
+            }
         }
     }
 }
