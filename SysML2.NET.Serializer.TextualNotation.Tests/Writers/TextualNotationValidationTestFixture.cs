f9d45f7c0df0c2704ecf2af85e177d578f318b1c
atheate <a.theate@stariongroup.eu>
2026-08-06 07:46:26 +0200

    3e validated

--- SysML2.NET.Serializer.TextualNotation.Tests/Writers/TextualNotationValidationTestFixture.cs
+++ SysML2.NET.Serializer.TextualNotation.Tests/Writers/TextualNotationValidationTestFixture.cs
@@ -46,6 +46,7 @@ namespace SysML2.NET.Serializer.TextualNotation.Tests.Writers
         [TestCase("03-Function-based Behavior", "3c-Function-based Behavior-structure mod-2.sysmlx")]
         [TestCase("03-Function-based Behavior", "3c-Function-based Behavior-structure mod-3.sysmlx")]
         [TestCase("03-Function-based Behavior", "3d-Function-based Behavior-item.sysmlx")]
+        [TestCase("03-Function-based Behavior", "3e-Function-based Behavior-item.sysmlx")]
         public async Task VerifyValidationTextualNotationXmi(string folderName, string fileName)
         {
             var loggerFactory = LoggerFactory.Create(builder =>
