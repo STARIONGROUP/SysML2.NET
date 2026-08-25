e0348c94e011b04567f493a343613d3a32495355
Antoine Théate <97221392+antoineatstariongroup@users.noreply.github.com>
2026-08-25 12:58:58 +0200

    fix #347 (#350)
    * fix #347
    
    * Fix SQ issues

--- SysML2.NET.Dal/Assembler.cs
+++ SysML2.NET.Dal/Assembler.cs
@@ -54,13 +54,20 @@ namespace SysML2.NET.Dal
         /// Gets the Cache that contains all the <see cref="Core.POCO.Root.Elements.IElement"/>s
         /// </summary>
         public ConcurrentDictionary<Guid, Lazy<Core.POCO.Root.Elements.IElement>> Cache { get; private set; }
-        
+
         /// <summary>
         /// Synchronize the Cache based on the provided <paramref name="dtos"/>
         /// </summary>
         /// <param name="dtos">
         /// the DTOs used to update the cache with
         /// </param>
+        /// <remarks>
+        /// When <paramref name="dtos"/> carries more than one DTO with the same identifier, the first occurrence
+        /// is the one that is added to the Cache and every subsequent duplicate is ignored.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="dtos"/> is null
+        /// </exception>
         public void Synchronize(IEnumerable<Core.DTO.Root.Elements.IElement> dtos)
         {
             if (dtos == null)
@@ -68,64 +75,86 @@ namespace SysML2.NET.Dal
                 throw new ArgumentNullException(nameof(dtos), $"The {nameof(dtos)} may not be null");
             }
 
+            // the DTOs are walked three times, materialize once so that a lazy sequence is not re-evaluated on every pass
+            var elements = dtos as IReadOnlyList<Core.DTO.Root.Elements.IElement> ?? dtos.ToList();
+
             var sw = Stopwatch.StartNew();
 
             var deletedIdentifiers = new List<Guid>();
 
             // update all POCOs based on provided DTOs, the result is a list unique identifiers of objects that may be removed
             this.logger.LogDebug("Update Value properties of POCO and Removed deleted Reference Properties");
-            foreach (var dto in dtos)
+
+            foreach (var dto in elements)
             {
                 if (this.Cache.TryGetValue(dto.Id, out var lazyPoco))
                 {
-                    var poco = lazyPoco.Value;
-                    var deletedPocos = poco.UpdateValueAndRemoveDeletedReferenceProperties(dto).ToList();
-                    deletedIdentifiers.AddRange(deletedPocos);
+                    deletedIdentifiers.AddRange(lazyPoco.Value.UpdateValueAndRemoveDeletedReferenceProperties(dto));
                 }
             }
-            this.logger.LogDebug("A total of {0} identifiers have been processed in {1} [ms] and ready to be deleted", deletedIdentifiers, sw.ElapsedMilliseconds);
+
+            if (this.logger.IsEnabled(LogLevel.Debug))
+            {
+                this.logger.LogDebug("A total of {DeletedCount} identifiers have been processed in {Elapsed} [ms] and ready to be deleted", deletedIdentifiers.Count, sw.ElapsedMilliseconds);
+            }
 
             // removed POCOs that are up for deletion
             foreach (var identifier in deletedIdentifiers)
             {
-                Lazy<Core.POCO.Root.Elements.IElement> lazyPoco;
-                if (this.Cache.TryRemove(identifier, out lazyPoco))
+                if (!this.Cache.TryRemove(identifier, out var deletedLazyPoco))
                 {
-                    this.logger.LogTrace("{0} with identifier {1} was deleted", lazyPoco.Value.GetType().Name, identifier);
+                    this.logger.LogWarning("The element with identifier {Identifier} was not deleted as it could not be found in the cache", identifier);
+                    continue;
                 }
-                else
+
+                if (this.logger.IsEnabled(LogLevel.Trace))
                 {
-                    this.logger.LogWarning("{0} with identifier {1} was not deleted as it could not be found in the cache", lazyPoco.Value.GetType().Name, identifier);
+                    this.logger.LogTrace("{PocoType} with identifier {Identifier} was deleted", deletedLazyPoco.Value.GetType().Name, identifier);
                 }
             }
 
             sw.Restart();
             this.logger.LogDebug("Add new POCOs to dictionary based on DTOs");
+
             var elementFactory = new ElementFactory();
-            var existingIdentifiers = this.Cache.Keys.ToList();
-            var dtoIdentifiers = dtos.Select(x => x.Id).ToList();
-            var newIdentifiers = dtoIdentifiers.Except(existingIdentifiers);
-            foreach (var identifier in newIdentifiers)
+            var addedCount = 0;
+
+            foreach (var dto in elements.Where(element => !this.Cache.ContainsKey(element.Id)))
             {
-                var dto = dtos.Single(x => x.Id == identifier);
                 var poco = elementFactory.Create(dto);
+
                 this.Cache.AddOrUpdate(poco.Id, new Lazy<Core.POCO.Root.Elements.IElement>(() => poco), (key, oldValue) => oldValue);
-                this.logger.LogTrace("{0}:{1} added to Cache", poco.GetType().Name, poco.Id);
+
+                addedCount++;
+
+                if (this.logger.IsEnabled(LogLevel.Trace))
+                {
+                    this.logger.LogTrace("{PocoType}:{Identifier} added to Cache", poco.GetType().Name, poco.Id);
+                }
             }
-            this.logger.LogDebug("A total of {0} POCOs have been added to the Cache in {1} [ms]", newIdentifiers.Count(), sw.ElapsedMilliseconds);
+
+            if (this.logger.IsEnabled(LogLevel.Debug))
+            {
+                this.logger.LogDebug("A total of {AddedCount} POCOs have been added to the Cache in {Elapsed} [ms]", addedCount, sw.ElapsedMilliseconds);
+            }
 
             sw.Restart();
             this.logger.LogDebug("Update POCO reference properties");
-            foreach (var dto in dtos)
+
+            foreach (var dto in elements)
             {
-                Lazy<Core.POCO.Root.Elements.IElement> lazyPoco;
-                if (this.Cache.TryGetValue(dto.Id, out lazyPoco))
+                if (this.Cache.TryGetValue(dto.Id, out var lazyPoco))
                 {
-                    var poco = lazyPoco.Value;
-                    poco.UpdateReferenceProperties(dto, this.Cache);
+                    lazyPoco.Value.UpdateReferenceProperties(dto, this.Cache);
                 }
             }
-            this.logger.LogDebug("POCO reference properties updated in {0} [ms]", sw.ElapsedMilliseconds);
+
+            if (this.logger.IsEnabled(LogLevel.Debug))
+            {
+                this.logger.LogDebug("POCO reference properties updated in {Elapsed} [ms]", sw.ElapsedMilliseconds);
+            }
+
+            sw.Stop();
         }
     }
 }
