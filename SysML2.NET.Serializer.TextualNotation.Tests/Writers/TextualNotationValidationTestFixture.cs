61432dc83e3a0e2efb693cd0546e474ec06cab41
atheate <a.theate@stariongroup.eu>
2026-08-31 12:26:50 +0200

    Fix #303

--- SysML2.NET.Serializer.TextualNotation.Tests/Writers/TextualNotationValidationTestFixture.cs
+++ SysML2.NET.Serializer.TextualNotation.Tests/Writers/TextualNotationValidationTestFixture.cs
@@ -134,6 +134,8 @@ namespace SysML2.NET.Serializer.TextualNotation.Tests.Writers
         [TestCase("15-Properties-Values-Expressions", "15_13-Discretely Sampled Function Value.sysmlx")]
         [TestCase("15-Properties-Values-Expressions", "15_19-Materials with Properties.sysmlx")]
         [TestCase("15-Properties-Values-Expressions", "15_19a-Materials with Properties.sysmlx")]
+        [TestCase("17-Sequence Modeling", "17a-Sequence-Modeling.sysmlx")]
+        [TestCase("17-Sequence Modeling", "17b-Sequence-Modeling.sysmlx")]
         public async Task VerifyValidationTextualNotationXmi(string folderName, string fileName)
         {
             var loggerFactory = LoggerFactory.Create(builder =>
