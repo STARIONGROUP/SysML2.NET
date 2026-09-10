6630dc190ec324617486e43b5092e996a1774286
atheate <a.theate@stariongroup.eu>
2026-09-10 10:06:21 +0200

    missing tests

---
+++ SysML2.NET.CodeGenerator.Tests/Expected/OpenAPI/PSM/AutoGenSerializer/BranchSerializer.cs
@@ -0,0 +1,132 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="BranchSerializer.cs" company="Starion Group S.A.">
+//
+//    Copyright (C) 2022-2026 Starion Group S.A.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
+
+namespace SysML2.NET.Serializer.Json.PSM
+{
+    using System;
+    using System.Text.Json;
+
+    using SysML2.NET.PSM.DTO;
+    using SysML2.NET.PSM.Enumerations;
+    using SysML2.NET.Serializer.Json;
+
+    /// <summary>
+    /// The purpose of the <see cref="BranchSerializer"/> is to provide serialization capabilities
+    /// for the <see cref="Branch"/> class
+    /// </summary>
+    internal static class BranchSerializer
+    {
+        /// <summary>
+        /// Serializes an instance of <see cref="Branch"/> using an <see cref="Utf8JsonWriter"/>
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="Branch"/> to serialize
+        /// </param>
+        /// <param name="writer">
+        /// The target <see cref="Utf8JsonWriter"/>
+        /// </param>
+        /// <param name="serializationModeKind">
+        /// enumeration specifying what kind of serialization shall be used
+        /// </param>
+        /// <param name="includeDerivedProperties">
+        /// Asserts that derived properties should also be part of the serialization
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the object is not a <see cref="Branch"/>
+        /// </exception>
+        /// <exception cref="NotSupportedException">
+        /// Thrown when the serialization mode is not supported
+        /// </exception>
+        internal static void Serialize(object obj, Utf8JsonWriter writer, SerializationModeKind serializationModeKind, bool includeDerivedProperties)
+        {
+            if (obj is not Branch branch)
+            {
+                throw new ArgumentException("The object shall be a Branch", nameof(obj));
+            }
+
+            if (serializationModeKind != SerializationModeKind.JSON)
+            {
+                throw new NotSupportedException($"The {serializationModeKind} serialization mode is not supported");
+            }
+
+            writer.WriteStartObject();
+
+            writer.WriteString("@type"u8, "Branch");
+
+            writer.WriteString("@id"u8, branch.Id);
+
+            writer.WriteStartArray("alias"u8);
+
+            if (branch.Alias != null)
+            {
+                foreach (var item in branch.Alias)
+                {
+                    writer.WriteStringValue(item);
+                }
+            }
+
+            writer.WriteEndArray();
+
+            writer.WriteString("created"u8, branch.Created);
+
+            if (branch.Deleted.HasValue)
+            {
+                writer.WriteString("deleted"u8, branch.Deleted.Value);
+            }
+            else
+            {
+                writer.WriteNull("deleted"u8);
+            }
+
+            writer.WriteString("description"u8, branch.Description);
+
+            if (branch.Head.HasValue)
+            {
+                writer.WriteStartObject("head"u8);
+                writer.WriteString("@id"u8, branch.Head.Value);
+                writer.WriteEndObject();
+            }
+            else
+            {
+                writer.WriteNull("head"u8);
+            }
+
+            writer.WriteString("name"u8, branch.Name);
+
+            writer.WriteStartObject("owningProject"u8);
+            writer.WriteString("@id"u8, branch.OwningProject);
+            writer.WriteEndObject();
+
+            writer.WriteStartObject("referencedCommit"u8);
+            writer.WriteString("@id"u8, branch.ReferencedCommit);
+            writer.WriteEndObject();
+
+            writer.WriteEndObject();
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
